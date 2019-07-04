5db9d59d935e4db22243844d6b9d4645869660d8
CLannadZSY <zsyMiDi@sina.com>
2019-07-04 18:03:31 +0800

    修改执行

--- train.py
+++ train.py
@@ -206,32 +206,6 @@ def train_crack_captcha_cnn():
 
             step += 1
 
+if __name__ == '__main__':
 
-train_crack_captcha_cnn()
-
-
-# 识别验证码
-# def crack_captcha(captcha_image):
-#     output = crack_captcha_cnn()
-#
-#     saver = tf.train.Saver()
-#     with tf.Session() as sess:
-#         saver.restore(sess, tf.train.latest_checkpoint('.'))
-#
-#         predict = tf.argmax(tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
-#         text_list = sess.run(predict, feed_dict={X: [captcha_image], keep_prob: 1})
-# 
-#         text = text_list[0].tolist()
-#         vector = np.zeros(MAX_CAPTCHA * CHAR_SET_LEN)
-#         i = 0
-#         for n in text:
-#             vector[i * CHAR_SET_LEN + n] = 1
-#             i += 1
-#         return vec2text(vector)
-#
-#
-# text, image = gen_captcha_text_and_image()
-# image = convert2gray(image)
-# image = image.flatten() / 255
-# predict_text = crack_captcha(image)
-# print("正确: {}  预测: {}".format(text, predict_text))
+    train_crack_captcha_cnn()
